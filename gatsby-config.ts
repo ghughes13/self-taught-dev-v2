12a14470d4b3f05974c9643caf67d48d8115b036
ghughes13 <battletank130@gmail.com>
2022-03-26 17:26:15 -0500

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,16 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "UA-175319701-1"
+    }
+  }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"]
+};
+
+export default config;
